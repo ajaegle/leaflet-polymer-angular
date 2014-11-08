dbfa31e25970449fc116fcfdf6a0f24a3af58126
ajaegle <mail@ajaegle.de>
2014-11-08 15:09:51 +0100

    add dummy implementation for geocoder service

--- app.js
+++ app.js
@@ -6,11 +6,25 @@ angular.module('polyhack', ['ng-polymer-elements']);
 
 angular.module('polyhack')
   .controller('MainCtrl',
-    function() {
+    function(GeoCoder) {
       this.location = {
         lat: 49.005120,
         lon: 8.414887
       };
       this.zoom = 14;
+      var _this = this;
+      this.performSearch = function(query) {
+        console.log(query);
+        _this.result = GeoCoder.request(query);
+      };
+    }
+  );
+
+angular.module('polyhack')
+  .service('GeoCoder',
+    function() {
+      this.request = function (query) {
+        return query + "!"
+      }
     }
   );
